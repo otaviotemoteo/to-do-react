9020f4e1ad62535d777985f3db1341aaf04511fb
otavio <otaviotemoteo@gmail.com>
2025-09-04 15:59:30 -0300

    feat: add tests components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import Icon from "./components/icon";
+import Spinner from "./assets/icons/spinner.svg?react";
+import Badge from "./components/badge";
+import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
-        <Text variant={"body-sm-bold"} className="text-pink-900">
-          Olá,mundo
-        </Text>
-        <Text className="text-green-500">Palmeiras</Text>
+      <div className="grid gap-3">
+        <div className="flex flex-col gap-1">
+          <Text variant={"body-sm-bold"} className="text-pink-900">
+            Olá,mundo
+          </Text>
+          <Text className="text-green-500">Palmeiras</Text>
+        </div>
+        <div className="flex gap-1">
+          <Icon svg={TrashIcon} />
+          <Icon svg={Spinner} animate />
+        </div>
+        <div>
+          <Badge variant={"secondary"}>5</Badge>
+          <Badge variant={"primary"}>2 de 5</Badge>
+        </div>
+        <div>
+          <Button>Nova tarefa</Button>
+        </div>
+
+        <div>
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant={"secondary"} />
+          <ButtonIcon icon={TrashIcon} variant={"tertiary"} />
+        </div>
       </div>
     </>
   );
