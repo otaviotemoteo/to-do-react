0512ffcde8f88e0f42040718a5f67ea4936f8fee
otavio <otaviotemoteo@gmail.com>
2025-09-05 15:03:53 -0300

    chore: add container component

---
+++ src/components/container.tsx
@@ -0,0 +1,36 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import React from "react";
+
+export const containerVariants = cva("mx-auto", {
+  variants: {
+    size: {
+      md: "max-w-[31.5rem] px-2",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+interface ContainerProps
+  extends VariantProps<typeof containerVariants>,
+    React.ComponentProps<"div"> {
+  as?: keyof React.JSX.IntrinsicElements;
+}
+
+export default function Container({
+  as = "div",
+  size,
+  children,
+  className,
+  ...props
+}: ContainerProps) {
+  return React.createElement(
+    as,
+    {
+      className: containerVariants({ size, className }),
+      ...props,
+    },
+    children
+  );
+}
