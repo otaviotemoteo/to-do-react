cd78f88d91a7fc7392adca9f7df4c6d1cc814117
otavio <otaviotemoteo@gmail.com>
2025-09-11 19:49:45 -0300

    fix: inputs style

--- src/core-components/tasks-summary.tsx
+++ src/core-components/tasks-summary.tsx
@@ -4,12 +4,14 @@ import Badge from "../components/badge"
 export default function TasksSumary() {
     return <>
     <div className="flex items-center gap-2">
-        <Text variant="body-sm-bold">Tarefas criadas</Text>
-        <Badge variant="secondary">5</Badge>
+        <Text variant="body-sm-bold" className="!text-gray-300">Tarefas criadas</Text>
+        <Badge variant="primary">5</Badge>
     </div>
     <div className="flex items-center gap-2">
-        <Text variant="body-sm-bold">Concluídas</Text>
-        <Badge variant="primary">2 de 5</Badge>
+        <Text variant="body-sm-bold" className="!text-gray-300">Concluídas</Text>
+        <Badge variant="secondary">
+            2 de 5
+        </Badge>
     </div>
     </>
 }
