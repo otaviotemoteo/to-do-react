67a94003c70f87eb8a3215d8186d0c27964a8e5f
otavio <otaviotemoteo@gmail.com>
2025-09-11 19:50:51 -0300

    feat: add tasksSumary to page home

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,3 +1,12 @@
+import Container from "../components/container";
+import TaskItem from "../core-components/task-item";
+import TasksSumary from "../core-components/tasks-summary";
+
 export default function PageHome() {
-  return <>Página home</>;
+  return <Container as="article" className="space-y-3">
+    <header className="flex items-center justify-between">
+      <TasksSumary />
+    </header>
+    <TaskItem></TaskItem>
+  </Container>
 }
