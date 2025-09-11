ebae69b4bc9262e315f2a481eca502eb22ad233a
otavio <otaviotemoteo@gmail.com>
2025-09-11 20:32:30 -0300

    feat: add tasks lists to page home

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,5 +1,5 @@
 import Container from "../components/container";
-import TaskItem from "../core-components/task-item";
+import TasksList from "../core-components/tasks-list";
 import TasksSumary from "../core-components/tasks-summary";
 
 export default function PageHome() {
@@ -7,6 +7,7 @@ export default function PageHome() {
     <header className="flex items-center justify-between">
       <TasksSumary />
     </header>
-    <TaskItem></TaskItem>
+    
+    <TasksList />
   </Container>
 }
