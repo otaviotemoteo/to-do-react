2c694f2f514aa43ed92ac63495fb3fed431ca3cd
otavio <otaviotemoteo@gmail.com>
2025-09-19 15:34:42 -0300

    feat: add dinamic tasks

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -1,24 +1,37 @@
-import ButtonIcon from "../components/button-icon";
-import PlusIcon from "../assets/icons/plus.svg?react"
+import Button from "../components/button";
+import PlusIcon from "../assets/icons/plus.svg?react";
 import TaskItem from "./task-item";
 import useTasks from "../hooks/use-tasks";
+import useTask from "../hooks/use-task";
+import { TaskState } from "../types/task";
 
 export default function TasksList() {
-    const {tasks} = useTasks();
+  const { tasks } = useTasks();
+  const { prepareTask } = useTask();
 
-    console.log(tasks);
+  function handleNewTask() {
+    prepareTask();
+  }
 
-    return (
+  console.log(tasks);
+
+  return (
     <>
-    <section>
-        <ButtonIcon icon={PlusIcon}>Nova tarefa</ButtonIcon>
-    </section>
-    <section className="space-y-2">
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
-    </section>
+      <section>
+        <Button
+          icon={PlusIcon}
+          className="w-full"
+          onClick={handleNewTask}
+          disabled={tasks.some((task) => task.state === TaskState.Creating)}
+        >
+          Nova tarefa
+        </Button>
+      </section>
+      <section className="space-y-2">
+        {tasks.map((task) => (
+          <TaskItem key={task.id} task={task} />
+        ))}
+      </section>
     </>
-    )
+  );
 }
