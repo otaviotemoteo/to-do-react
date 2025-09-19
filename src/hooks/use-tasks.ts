676f326fe0a99ab080ed414b97c97d32c490620c
otavio <otaviotemoteo@gmail.com>
2025-09-19 15:35:45 -0300

    chore: add concluded filter

--- src/hooks/use-tasks.ts
+++ src/hooks/use-tasks.ts
@@ -1,13 +1,13 @@
 import useLocalStorage from "use-local-storage";
-import { TASKS_KEY } from "../types/task";
-import type {Task} from "../types/task"
+import { TASKS_KEY, TaskState } from "../types/task";
+import type { Task } from "../types/task";
 
 export default function useTasks() {
-    const [tasks] = useLocalStorage<Task[]>(TASKS_KEY, []);
+  const [tasks] = useLocalStorage<Task[]>(TASKS_KEY, []);
 
-    return {
-        tasks,
-        tasksCount: tasks.length,
-        concludedTasksCount: tasks.filter((task) => task.concluded).length
-    }
+  return {
+    tasks,
+    tasksCount: tasks.filter((task) => task.state === TaskState.Created).length,
+    concludedTasksCount: tasks.filter((task) => task.concluded).length,
+  };
 }
